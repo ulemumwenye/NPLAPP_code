bbb0804833dd021e6078f37bf6198fcae93eaba3
ulemumwenye <113883746+ulemumwenye@users.noreply.github.com>
2024-01-23 20:55:11 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'npl',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
