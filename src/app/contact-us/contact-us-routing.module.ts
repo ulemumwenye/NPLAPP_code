9f4dfd205a710d4219d54564a606e2564c5dac83
ulemumwenye <113883746+ulemumwenye@users.noreply.github.com>
2024-01-25 12:35:31 +0200

    update
    updated contact us page

---
+++ src/app/contact-us/contact-us-routing.module.ts
@@ -0,0 +1,19 @@
+// src/app/contact-us/contact-us-routing.module.ts
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ContactUsPage } from './contact-us.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ContactUsPage
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ContactUsPageRoutingModule {}
